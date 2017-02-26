6b0fb10cefd13f5f262a2a7bc9a9932b65522acb
haelbouhad <hassan.bouhadi@gmail.com>
2017-02-26 07:25:33 +0100

    Add means Histogram

--- src/UV_Characterization.java
+++ src/UV_Characterization.java
@@ -4,25 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ij.*;
+import ij.gui.HistogramWindow;
+import ij.gui.NewImage;
 import ij.gui.Roi;
 import ij.measure.ResultsTable;
 import ij.plugin.*;
+import ij.plugin.filter.PlugInFilter;
 import ij.plugin.frame.RoiManager;
+import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 
-public class UV_Characterization implements PlugIn {
+public class UV_Characterization implements PlugInFilter {
 	
 	private List<Measurment> measurments;
+	
+	private ImagePlus imp;
 
 	public UV_Characterization() {
+		
 		measurments = new ArrayList<Measurment>();
+		
 	}
 	
-	public void run(String arg) {
+	public int setup(String arg, ImagePlus imp)
+	{
+		/*try
+		{
+			if (imp == null) throw (new RuntimeException());
+			this.imp = imp;
+		}
+		catch (RuntimeException e)
+		{
+			IJ.error("this plugin requires an ROI image");
+		}
+		*/
+		return DOES_RGB;
+	}
+	
+	@Override
+	public void run(ImageProcessor arg0) {
         
         	
         	IJ.open("/home/hassan/Documents/ZZ3/PROJET-ANNEE/PROJET/MYSAMPLE/SEQUENCe/SEQ/E-2/RoiSet.zip");
-        	ImagePlus imp = IJ.openImage("/home/hassan/Documents/ZZ3/PROJET-ANNEE/PROJET/MYSAMPLE/SEQUENCe/SEQ/E-2/b-contrasted-roi-overlay.tif");
+        	imp = IJ.openImage("/home/hassan/Documents/ZZ3/PROJET-ANNEE/PROJET/MYSAMPLE/SEQUENCe/SEQ/E-2/b-contrasted-roi-overlay.tif");
     
         	
         	
@@ -45,26 +69,41 @@ public class UV_Characterization implements PlugIn {
     										measurement.getStringValue(3, 0)
     							));
     			
-    			/*
-    			IJ.log(measurement.getLabel(0) + " --- " + measurement.getStringValue(0, 0) + " -- " + measurement.getStringValue(1, 0) + " -- " 
-    					+ measurement.getStringValue(2, 0) + " -- " + measurement.getStringValue(3, 0));
-    			*/
     		}
     		
     		imp.show();
-    		  		
+    		
+    		/* HISTOGRAMME */
+    		  	
+    		ImagePlus imp3 = NewImage.createFloatImage("means image", measurments.size(), 1, 1, 1);
+    		
+    		FloatProcessor fp = (FloatProcessor) imp3.getProcessor();
+    		
+    		for(Measurment measurment : measurments){
+    			fp.setf(measurment.getId(), 0, measurment.getMean());
+    			IJ.log(measurment.getId() + " - " + 0 +" - " + measurment.getMean());
+    		}
+    		
+    		
+    		HistogramWindow histo = new HistogramWindow("Means histogram", imp3, 256);
 	    	
-			roi.setSelectedIndexes(roi.getIndexes());
+			/*roi.setSelectedIndexes(roi.getIndexes());
 			roi.runCommand("Set Fill Color", "yellow");
 			
 			roi.runCommand(imp,"Show All");
 			roi.runCommand(imp,"Show All with labels");
 			roi.deselect();
 			roi.select(0);
-			roi.runCommand("Set Fill Color", "red"); 
+			roi.runCommand("Set Fill Color", "red"); */
+			
+			
+			
+			
     		
     		
 	}
+
+
 
 }	
 
@@ -90,6 +129,10 @@ class Measurment {
 		this.mean  = Float.parseFloat(mean);
 		this.min   = Float.parseFloat(min);
 		this.max   = Float.parseFloat(max);
+	}
+	
+	public int getId() {
+		return id;
 	}
 
 	public float getArea() {
