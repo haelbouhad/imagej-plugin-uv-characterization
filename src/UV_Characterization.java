141548d3f2d66b4f28de2d924c9cad52da2f0413
haelbouhad <hassan.bouhadi@gmail.com>
2017-02-27 03:28:48 +0100

    Color specified classes

--- src/UV_Characterization.java
+++ src/UV_Characterization.java
@@ -3,13 +3,10 @@
 import java.awt.AWTEvent;
 import java.awt.Color;
 import java.awt.Scrollbar;
-import java.awt.TextField;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
-import javax.swing.Scrollable;
 
 import ij.*;
 import ij.gui.DialogListener;
@@ -17,10 +14,8 @@ import ij.gui.GenericDialog;
 import ij.gui.HistogramWindow;
 import ij.gui.NewImage;
 import ij.gui.NonBlockingGenericDialog;
-import ij.gui.Roi;
 import ij.measure.Measurements;
 import ij.measure.ResultsTable;
-import ij.plugin.*;
 import ij.plugin.filter.PlugInFilter;
 import ij.plugin.frame.RoiManager;
 import ij.process.FloatProcessor;
@@ -39,6 +34,8 @@ public class UV_Characterization implements PlugInFilter, DialogListener {
 	private int nbClasses;
 	
 	private List<Classe> classes;
+	
+	private String[] color = { "RED" , "YELLOW" , "GREEN" };
 	
 	
 	private int threshold[];
@@ -74,30 +71,10 @@ public class UV_Characterization implements PlugInFilter, DialogListener {
         	
         	IJ.open("/home/hassan/Documents/ZZ3/PROJET-ANNEE/PROJET/MYSAMPLE/SEQUENCe/SEQ/E-2/RoiSet.zip");
         	imp = IJ.openImage("/home/hassan/Documents/ZZ3/PROJET-ANNEE/PROJET/MYSAMPLE/SEQUENCe/SEQ/E-2/b-contrasted-roi-overlay.tif");
-    
-        	
-        	
     		rm = RoiManager.getRoiManager();
     		
-    		
-    		ResultsTable measurement =  null;
-    		
-    		for(int i = 0; i < rm.getCount() ; ++i){
-    			
-    			rm.select(imp, i);		
-    			
-    			measurement = rm.multiMeasure(imp);
-    			
-    			measurments.add(new Measurment(i, 
-    									    measurement.getLabel(0), 
-    										measurement.getStringValue(0, 0), 
-    										measurement.getStringValue(1, 0), 
-    										measurement.getStringValue(2, 0), 
-    										measurement.getStringValue(3, 0)
-    							));
-    			
-    			
-    		}
+    		// Get measurments from roi manager
+    		fillMeasurments();
     		
     		imp.show();
     		
@@ -120,23 +97,60 @@ public class UV_Characterization implements PlugInFilter, DialogListener {
 					break;
 			}
 
+    		// Fill classes
+    		makeClasses();
+    		fillClasses();
     		
-    		
-	    	
-			/*roi.setSelectedIndexes(roi.getIndexes());
-			roi.runCommand("Set Fill Color", "yellow");
-			
-			roi.runCommand(imp,"Show All");
-			roi.runCommand(imp,"Show All with labels");
-			roi.deselect();
-			roi.select(0);
-			roi.runCommand("Set Fill Color", "red"); */
-			
-			
-			
+    		// Color Cells
+    		colorClasses();			
 			
     		
     		
+	}
+	
+	public void fillMeasurments(){
+		ResultsTable measurement =  null;
+		
+		for(int i = 0; i < rm.getCount() ; ++i){
+			
+			rm.select(imp, i);		
+			
+			measurement = rm.multiMeasure(imp);
+			
+			measurments.add(new Measurment(i, 
+									    measurement.getLabel(0), 
+										measurement.getStringValue(0, 0), 
+										measurement.getStringValue(1, 0), 
+										measurement.getStringValue(2, 0), 
+										measurement.getStringValue(3, 0)
+							));
+			
+			
+		}	
+	}
+
+	public void colorClasses(){
+    	for(Classe classe : classes){
+    		rm.setSelectedIndexes(classe.getIndexes());
+			rm.runCommand("Set Fill Color", color[classes.indexOf(classe)]);
+			rm.runCommand(imp,"Show All");
+			rm.runCommand(imp,"Show All with labels");
+    	}
+	}
+	
+	private void fillClasses() {
+		
+		for(Measurment measurment : measurments){
+			float mean = measurment.getMean();
+			for( int i = 0 ; i < nbClasses ; ++i){
+				if( classes.get(i).getMinThreshold() <= mean  && mean < classes.get(i).getMaxThreshold() ){
+					classes.get(i).addIndex(measurment.getId());
+					//IJ.log(classes.get(i).getMinThreshold() + " - " + mean + " - " + classes.get(i).getMaxThreshold());
+				}
+			}
+		}
+		
+		
 	}
 
 	private void makeHistogram() {
@@ -147,7 +161,7 @@ public class UV_Characterization implements PlugInFilter, DialogListener {
 		
 		for(Measurment measurment : measurments){
 			fp.setf(measurment.getId(), 0, measurment.getMean());
-			IJ.log(measurment.getId() + " - " + 0 +" - " + measurment.getMean());
+			//IJ.log(measurment.getId() + " - " + 0 +" - " + measurment.getMean());
 		}
 		
 		histo = new HistogramWindow("Means histogram", imp3, 256);
@@ -189,7 +203,6 @@ public class UV_Characterization implements PlugInFilter, DialogListener {
 		if(dial2.wasCanceled()){
 			return;
 		}
-		makeClasses();
 		
 	}
 
@@ -205,15 +218,20 @@ public class UV_Characterization implements PlugInFilter, DialogListener {
 		if(dial2.wasCanceled()){
 			return;
 		}
-		makeClasses();	
+			
 	}
 
 	private void makeClasses() {
-		classes.add(new Classe(Measurment.minMean, threshold[0]));
-		for(int i = 2 ; i < nbClasses ; ++i){
-			classes.add(new Classe(threshold[i-2], threshold[i-1]));
+		
+		if(nbClasses == 1){
+			classes.add(new Classe(Measurment.minMean, Measurment.maxMean));
+		}else{
+			classes.add(new Classe(Measurment.minMean, threshold[0]));
+			for(int i = 2 ; i < nbClasses ; ++i){
+				classes.add(new Classe(threshold[i-2], threshold[i-1]));
+			}
+			classes.add(new Classe(threshold[nbClasses-2], Measurment.maxMean));
 		}
-		classes.add(new Classe(threshold[nbClasses-2], Measurment.maxMean));
 	}
 
 	@Override
@@ -328,16 +346,58 @@ class Classe {
 	private float minThreshold;
 	
 	private float maxThreshold;
+	
+	private List<Integer> indexes;
+
+
+	public int[] getIndexes() {
+		int[] tab = new int[indexes.size()];
+		for(int i = 0 ; i < tab.length ; ++i)
+			tab[i] = indexes.get(i);
+		return tab;
+	}
 
 
 	public Classe(float minThreshold, float maxThrashold) {
 		super();
-		this.minThreshold = minThreshold;
-		this.maxThreshold = maxThrashold;
-		IJ.log(minThreshold + " ** " + maxThrashold );
+		this.setMinThreshold(minThreshold);
+		this.setMaxThreshold(maxThrashold);
+		indexes = new ArrayList<Integer>();
 	}
 
-	
+
+	public void addIndex(int id) {
+		indexes.add(id);
+	}
+
+
+	public float getMinThreshold() {
+		return minThreshold;
+	}
+
+
+	public void setMinThreshold(float minThreshold) {
+		this.minThreshold = minThreshold;
+	}
+
+
+	public float getMaxThreshold() {
+		return maxThreshold;
+	}
+
+
+	public void setMaxThreshold(float maxThreshold) {
+		this.maxThreshold = maxThreshold;
+	}
+
+	@Override
+	public String toString() {
+		String str = "[ Min : " + minThreshold + " - Max : " + maxThreshold + "\n  Indexes = {";
+		for(Integer index : indexes)
+			str += index.toString() + ", ";
+		str += "}\n]";
+		return str;
+	}
 	
 }
 
